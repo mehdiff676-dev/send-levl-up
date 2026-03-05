2b8efdd4873da83b9b980cb3048fb045a09910b9
mehdiff676-dev <mehdiff676@gmail.com>
2026-03-06 00:33:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # ========== الإعدادات ==========
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8634923674:AAFK3q-9prlbYx-jDnyvMrDIAlP8JJuc-8E")
-CHAT_ID = os.environ.get("CHAT_ID", "6850087673")
+CHAT_ID = os.environ.get("CHAT_ID", "8203901188")
 # ===============================
 
 def get_real_ip(request):
